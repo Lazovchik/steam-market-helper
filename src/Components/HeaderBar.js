6b69d9f3317ab36ba7d3b491fea16ac9413de134
lazovchik <lazovchik@gmail.com>
2020-12-22 13:04:30 +0100

    Default structure defined

---
+++ src/Components/HeaderBar.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import {Container} from "react-bootstrap";
+import logo from "../logo.svg";
+
+class HeaderBar extends Component {
+    render() {
+        return (
+            <Container>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </Container>
+        );
+    }
+}
+
+export default HeaderBar;
