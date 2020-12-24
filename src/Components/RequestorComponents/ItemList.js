d165386d57d3f2a2f630466ad3586294d1333db6
lazovchik <lazovchik@gmail.com>
2020-12-24 14:24:20 +0100

    Local Storage done

--- src/Components/RequestorComponents/ItemList.js
+++ src/Components/RequestorComponents/ItemList.js
@@ -2,9 +2,77 @@ import React, {Component} from "react";
 import {Button, ListGroup, Row, Col} from "react-bootstrap";
 
 class ItemList extends Component {
+    constructor(props) {
+
+        super(props);
+
+        //this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            wList:''
+        }
+
+    }
+
+    componentDidMount() {
+        var values = [],
+            keys = Object.keys(localStorage),
+            i = keys.length;
+
+        while ( i-- ) {
+            values.push( localStorage.getItem(keys[i]) );
+        }
+        const val = values
+
+        this.setState({ wList: val })
+        console.log(val)
+        console.log(this.state)
+    }
+
+    handleClick = () => {
+
+        var values = [],
+            keys = Object.keys(localStorage),
+            i = keys.length;
+
+        while ( i-- ) {
+            values.push( localStorage.getItem(keys[i]) );
+        }
+        const val = values
+
+        this.setState({ wList: val })
+        console.log(this.state.wList)
+    }
+
+    listRender (arr) {
+        let list = []
+        for (let i = 0; i <arr.length; i++)
+        {
+            list.push (
+                <ListGroup.Item>
+                    {arr[i]}
+                    <Button variant='warning' type='submit' className='float-right btn-sm'>
+                        Remove
+                    </Button>
+                </ListGroup.Item>
+            )
+        }
+        return list
+    }
     render() {
+        const arr = this.state.wList
+        /*const listItems = arr.map((weapon) =>
+                <ListGroup.Item>
+                    {weapon}
+                    <Button variant='warning' type='submit' className='float-right btn-sm'>
+                        Remove
+                    </Button>
+                </ListGroup.Item>
+        )*/
+
         return (
-            <ListGroup className='bg-dark mt-4 p-3 rounded'>
+            <ListGroup className='bg-dark mt-4 p-3 rounded' >
+                {this.listRender(arr)}
                 <ListGroup.Item>
                     A
                     <Button variant='warning' type='submit' className='float-right btn-sm'>
@@ -17,6 +85,9 @@ class ItemList extends Component {
                         Remove
                     </Button>
                 </ListGroup.Item>
+                <Button  variant='warning' type="submit" className='mt-3' onClick={this.handleClick}>
+                    Update
+                </Button>
             </ListGroup>
         );
     }
--- src/Components/RequestorComponents/RequestorForm.js
+++ src/Components/RequestorComponents/RequestorForm.js
@@ -4,17 +4,52 @@ import React, {Component} from "react";
 import {Form, Button} from "react-bootstrap";
 
 class RequestorForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangeState = this.onChangeState.bind(this);
+        this.onChangeTrack = this.onChangeTrack.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            wName: '',
+            wState: '',
+            wTrack: '',
+            wPrice:''
+        }
+    }
+
+    // Form Events
+    onChangeName(e) {
+        this.setState({ wName: e.target.value })
+    }
+
+    onChangeState(e) {
+        this.setState({ wState: e.target.value })
+    }
+
+    onChangeTrack(e) {
+        this.setState({ wTrack: e.target.value })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+        localStorage.setItem(JSON.stringify(this.state), JSON.stringify(this.state));
+    }
+
     render() {
         return (
-            <Form className='bg-dark mt-4 p-3 rounded'>
+            <Form className='bg-dark mt-4 p-3 rounded' onSubmit={this.onSubmit}>
                 <Form.Group controlId="ItemName">
                     <Form.Label className='text-warning'>Items Name</Form.Label>
-                    <Form.Control type="text" placeholder="Weapon | Name " />
+                    <Form.Control type="text" placeholder="Weapon | Name" value={this.state.wName} onChange={this.onChangeName} />
                     <Form.Text className="text-muted">
                         Example: AK-47 | Phantom Disruptor
                     </Form.Text>
                 </Form.Group>
-                <Form.Group controlId="ItemState">
+                <Form.Group controlId="ItemState" value='Field-Tested' onChange={this.onChangeState} >
                     <Form.Control as="select">
                         <option>Battle-Scarred</option>
                         <option>Well-Worn</option>
@@ -30,6 +65,7 @@ class RequestorForm extends Component {
                             label="StatTrak™"
                             name="radio"
                             id="radioStatTrack"
+                            onChange={this.onChangeTrack}
                         />
                         <Form.Check
                             type="radio"
